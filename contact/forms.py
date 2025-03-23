7dbc9b5e7336c1abdde44d0d0b0a26ef9f63704d
xjucelio <jucelio_santos@hotmail.com>
2025-03-23 16:55:59 +0000

    feat: add user registration functionality with form and template

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from contact.models import Contact  # type: ignore
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 class ContactForm(forms.ModelForm):
@@ -72,3 +74,25 @@ class ContactForm(forms.ModelForm):
                     code='invalid'))
 
         return first_name
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    email = forms.EmailField()
+
+    class Meta():
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error('email', ValidationError(
+                'Ja existe este email', code='invalid'))
+
+        return email
---
+++ contact/views/user_forms.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from contact.forms import RegisterForm
+from django.contrib import messages
+
+
+def register(request):
+    form = RegisterForm()
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Usuario registrado')
+            return redirect('contact:index')
+
+    return render(
+        request,
+        'contact/register.html',
+        {
+            'form': form
+        }
+    )
