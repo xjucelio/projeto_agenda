6aff8b2740c0869b3b49703acb5edb90b7fbcbf7
xjucelio <jucelio_santos@hotmail.com>
2025-03-26 15:20:46 +0000

    feat: add user profile update functionality with forms and templates

--- contact/forms.py
+++ contact/forms.py
@@ -1,52 +1,28 @@
 from django import forms
-from django.core.exceptions import ValidationError
-from contact.models import Contact  # type: ignore
+from django.contrib.auth import password_validation
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from .models import Contact  # type: ignore
 
 
 class ContactForm(forms.ModelForm):
     picture = forms.ImageField(
         widget=forms.FileInput(
             attrs={
-                'accept': 'image/*'
+                'accept': 'image/*',
             }
-        )
+        ),
+        required=False
     )
-    # first_name = forms.CharField(
-    #     widget=forms.TextInput(
-    #         attrs={
-    #             'class': 'classe-a classe-b',
-    #             'placeholder': 'Aqui veio do forms',
-    #         }
-    #     ),
-    #     label='Primeiro Nome',
-    #     help_text='Texto de ajuda para seu usuário',
-    # )
-
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-
-    #     # self.fields['first_name'].widget.attrs.update({
-    #     #     'class': 'classe-a classe-b',
-    #     #     'placeholder': 'Aqui veio do init',
-    #     # })
 
     class Meta:
         model = Contact
         fields = (
             'first_name', 'last_name', 'phone',
             'email', 'description', 'category',
-            'picture'
+            'picture',
         )
-        # widgets = {
-        #     'first_name': forms.TextInput(
-        #         attrs={
-        #             'class': 'classe-a classe-b',
-        #             'placeholder': 'Escreva aqui'
-        #         }
-        #     )
-        # }
 
     def clean(self):
         cleaned_data = self.cleaned_data
@@ -55,7 +31,7 @@ class ContactForm(forms.ModelForm):
 
         if first_name == last_name:
             msg = ValidationError(
-                'Primeiro nome nao pode ser igual ao segundo',
+                'Primeiro nome não pode ser igual ao segundo',
                 code='invalid'
             )
             self.add_error('first_name', msg)
@@ -71,17 +47,25 @@ class ContactForm(forms.ModelForm):
                 'first_name',
                 ValidationError(
                     'Veio do add_error',
-                    code='invalid'))
+                    code='invalid'
+                )
+            )
 
         return first_name
 
 
 class RegisterForm(UserCreationForm):
-    first_name = forms.CharField(required=True)
-    last_name = forms.CharField(required=True)
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
     email = forms.EmailField()
 
-    class Meta():
+    class Meta:
         model = User
         fields = (
             'first_name', 'last_name', 'email',
@@ -92,7 +76,102 @@ class RegisterForm(UserCreationForm):
         email = self.cleaned_data.get('email')
 
         if User.objects.filter(email=email).exists():
-            self.add_error('email', ValidationError(
-                'Ja existe este email', code='invalid'))
+            self.add_error(
+                'email',
+                ValidationError('Já existe este e-mail', code='invalid')
+            )
 
         return email
+
+
+class RegisterUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='Required.',
+        error_messages={
+            'min_length': 'Please, add more than 2 letters.'
+        }
+    )
+    last_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='Required.'
+    )
+
+    password1 = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        help_text=password_validation.password_validators_help_text_html(),
+        required=False,
+    )
+
+    password2 = forms.CharField(
+        label="Password 2",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        help_text='Use the same password as before.',
+        required=False,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name',
+        )
+
+    def save(self, commit=True):
+        cleaned_data = self.cleaned_data
+        user = super().save(commit=False)
+        password = cleaned_data.get('password1')
+
+        if password:
+            user.set_password(password)
+
+        if commit:
+            user.save()
+
+        return user
+
+    def clean(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        if password1 or password2:
+            if password1 != password2:
+                self.add_error(
+                    'password2',
+                    ValidationError('Senhas não batem')
+                )
+
+        return super().clean()
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        current_email = self.instance.email
+
+        if current_email != email:
+            if User.objects.filter(email=email).exists():
+                self.add_error(
+                    'email',
+                    ValidationError('Já existe este e-mail', code='invalid')
+                )
+
+        return email
+
+    def clean_password1(self):
+        password1 = self.cleaned_data.get('password1')
+
+        if password1:
+            try:
+                password_validation.validate_password(password1)
+            except ValidationError as errors:
+                self.add_error(
+                    'password1',
+                    ValidationError(errors)
+                )
+
+        return password1
